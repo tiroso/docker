c1856907872a63a3535d4bc1af64320aa4ca1496
tiroso <sobisch.t@gmail.com>
2018-05-21 20:29:14 +0200

    Update start.sh

--- fhem-server/start.sh
+++ fhem-server/start.sh
@@ -4,14 +4,6 @@ set -e
 cd /opt/fhem
 port=7072
 
-while true; do
-    read -p "Do you wish to install this program?" yn
-    case $yn in
-        [Yy]* ) echo yes; break;;
-        [Nn]* ) echo no; break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
 
 if [ "$FHEM_SQLITE" = 1 ];
 then 
