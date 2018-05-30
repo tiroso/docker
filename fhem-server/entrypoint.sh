3d6c1b2a8e0267664ad88c901bb37e3a01497fac
tiroso <sobisch.t@gmail.com>
2018-05-31 01:11:04 +0200

    Update entrypoint.sh

--- fhem-server/entrypoint.sh
+++ fhem-server/entrypoint.sh
@@ -11,14 +11,14 @@ else
 mv /opt/fhemorigin/* /opt/fhem;
 rm /opt/fhemorigin -R;
 echo 'define InstallRoutine notify global:INITIALIZED sleep 1;;delete InstallRoutine;;save;;update;;sleep 1;;shutdown' >> /opt/fhem/fhem.cfg;
-/usr/bin/perl fhem.pl -d /opt/fhem/fhem.cfg | tee /opt/fhem/log/fhem_init_start.log;
+perl fhem.pl -d fhem.cfg | tee /opt/fhem/log/fhem_init_start.log;
 fi;
 
 if [ "$1" = "configdb" ];
 then 
 echo "Starte FHEM - configDB";
-/usr/bin/perl /opt/fhem/fhem.pl configDB;
+perl fhem.pl configDB;
 else
 echo "Starte FHEM";
-/usr/bin/perl /opt/fhem/fhem.pl /opt/fhem/fhem.cfg;
+perl fhem.pl fhem.cfg;
 fi;
